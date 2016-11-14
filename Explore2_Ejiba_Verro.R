ca1dec64c8af934f66674baa29b8afdbae16dbb8
Verro <vme8@georgetown.edu>
2016-11-14 15:02:12 -0500

    HW 8

---
+++ Explore2_Ejiba_Verro.R
@@ -0,0 +1,8 @@
+#Verro Ejiba
+#Homework 8 
+#Group C
+#Think about the ways things could go wrong and write some defensive code to gracefully handle exceptions. 
+#(for example,what if the first parameter is not a dataframe?)
+
+source("Explore_Ejiba_Verro.R")
+explore(dataframe, plot_switch = 'ON', threshold = 0, bins = 2)
