d1b96dd9fda3915258b8e9ff641102c42c32367e
Verro <vme8@georgetown.edu>
2016-11-17 15:08:37 -0500

    done

--- Explore2_Ejiba_Verro.R
+++ Explore2_Ejiba_Verro.R
@@ -6,24 +6,26 @@
 
 #Refers to the main Explore function from a different R Scrit
 source("Explore_Ejiba_Verro.R")
-explore(dataframe, plot_switch = 'ON', threshold = 0, bins = 2)
-#We need an error message when the function parameters are missing
-if (missing(dataframe))
-  stop("Need to input a valid data frame.")
-if (missing(plot_switch))
-  stop("Need to enter the mode for plot in order to generate graphs.")
-if (missing(threshold))
-  stop("Need to enter the correct threshold for correlation computation")
-if (missing(bins))
-  stop("Need to enter the right number of bins for the plots")
-#Need to have error message when an parameter that has been entered is not a data frame
-if(is.data.frame(dataframe) == FALSE)
-  stop("Please enter a valid dataframe")
-#wrong value for plot_switch
-if(plot_switch != "on" & plot_switch != "off" & plot_switch != "grid")
-      stop("Please enter 'on', 'off', or 'grid' for the plot mode")
-#Non numerical input for threshold and bins
-if(is.numeric(threshold) == FALSE)
-  stop("Enter a numberical value for the threshold")
-if(is.numeric(bins) == FALSE)
-  stop("Please enter a numerical bin value")
+explore(dataframe, plot_switch, threshold, bins){
+
+  #We need an error message when the function parameters are missing
+  if (missing(dataframe))
+    stop("Need to input a valid data frame.")
+  if (missing(plot_switch))
+    stop("Need to enter the mode for plot in order to generate graphs.")
+  if (missing(threshold))
+    stop("Need to enter the correct threshold for correlation computation")
+  if (missing(bins))
+    stop("Need to enter the right number of bins for the plots")
+  #Need to have error message when an parameter that has been entered is not a data frame
+  if(is.data.frame(dataframe) == FALSE)
+    stop("Please enter a valid dataframe")
+  #wrong value for plot_switch
+  if(plot_switch != "on" & plot_switch != "off" & plot_switch != "grid")
+    stop("Please enter 'on', 'off', or 'grid' for the plot mode")
+  #Non numerical input for threshold and bins
+  if(is.numeric(threshold) == FALSE)
+    stop("Enter a numberical value for the threshold")
+  if(is.numeric(bins) == FALSE)
+    stop("Please enter a numerical bin value") 
+}
