ecd0618e66c2c5196569ff56ed03af3049e45e68
Verro <vme8@georgetown.edu>
2016-11-17 09:48:12 -0500

    update

--- Explore2_Ejiba_Verro.R
+++ Explore2_Ejiba_Verro.R
@@ -6,3 +6,10 @@
 
 source("Explore_Ejiba_Verro.R")
 explore(dataframe, plot_switch = 'ON', threshold = 0, bins = 2)
+#Users can forget to enter all the parameters
+#We need an error message when the dataframe parameter is missing
+if (missing(dataframe))
+  stop("Need to input a valid data frame.")
+#Need to have error message when an parameter that has been entered is not a dataframe
+if(!is.data.frame(dataframe))
+  stop("Please enter a valid dataframe")
